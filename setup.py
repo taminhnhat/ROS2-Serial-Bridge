4e32dffdd2ee977fcdf0a41bed1105620aaa606e
taminhnhat <taminhnhat98@gmail.com>
2023-07-07 16:15:12 +0700

    i

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'serial = serial_bridge.main:main',
+            'plotter = serial_bridge.plotter:main',
         ],
     },
 )
